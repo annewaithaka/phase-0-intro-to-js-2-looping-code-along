c42268e43b5079635baa204eef50fd22afcda445
annewaithaka <annewaithaka68@gmail.com>
2024-07-04 01:41:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+}
+
+// Function to count down from a number to 0
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log (i);
+  }
+}
